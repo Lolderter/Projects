9def449ef6223740c3a984e93b0811f80c191374
Lolderter <152001412+Lolderter@users.noreply.github.com>
2025-04-08 10:10:03 +0200

    Add files via upload

---
+++ math/main.js
@@ -0,0 +1,24 @@
+
+function round(){
+    
+    var liczba = parseFloat(document.getElementById("pole").value)
+
+    if(Number.isInteger(liczba)) {
+        alert("Wrong number")
+        return;
+
+    }
+    var wynikceil = Math.ceil(liczba)
+    var wynikfloor = Math.floor(liczba)
+    var wynikround= Math.round(liczba)
+    var wyniktrunc = Math.trunc(liczba)
+
+
+
+
+
+document.getElementById("wynikceil").innerHTML = wynikceil
+document.getElementById("wynikfloor").innerHTML = wynikfloor
+document.getElementById("wynikround").innerHTML = wynikround
+document.getElementById("wyniktrunc").innerHTML = wyniktrunc
+}
