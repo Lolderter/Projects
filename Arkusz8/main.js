91a63999f4eafc5b642c5c454e4c0ee83cfeffef
Lolderter <152001412+Lolderter@users.noreply.github.com>
2025-06-04 10:50:36 +0200

    Add files via upload

---
+++ Arkusz8/main.js
@@ -0,0 +1,28 @@
+function oblicz() {
+    var wybranaDlugosc = document.querySelector('input[name="dlugosc"]:checked');
+    if (!wybranaDlugosc) {
+        alert("Wybierz długość włosów!");
+        return;
+    }
+
+    var cenaBazowa;
+
+    if (wybranaDlugosc.value === "25") {
+        cenaBazowa = 25;
+    } else if (wybranaDlugosc.value === "30") {
+        cenaBazowa = 30;
+    } else if (wybranaDlugosc.value === "40") {
+        cenaBazowa = 40;
+    } else if (wybranaDlugosc.value === "50") {
+        cenaBazowa = 50;
+    } else {
+        alert("Nieprawidłowa wartość!");
+        return;
+    }
+
+    var cenaPromocyjna = cenaBazowa - 10;
+   
+
+    var wynik = document.getElementById("wynik").innerHTML = "Cena promocyjna:"+cenaPromocyjna;
+ 
+}
